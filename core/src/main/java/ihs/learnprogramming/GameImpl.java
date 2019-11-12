b5bfce5a8988c688b2dde4255c3e5a8841108aa4
Excelnd <ihsanmpm@gmail.com>
2019-11-12 17:46:23 +0530

    Code clean up & Constructor Injuction

--- core/src/main/java/ihs/learnprogramming/GameImpl.java
+++ core/src/main/java/ihs/learnprogramming/GameImpl.java
@@ -8,18 +8,16 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
+@Component
 public class GameImpl implements Game {
 
     // -- constants --
     private static final Logger log = LoggerFactory.getLogger(GameImpl.class);
 
     // -- fields --
-    @Autowired
-    private NumberGenerator numberGenerator;
+    private final NumberGenerator numberGenerator;
 
-    @Autowired
-    @GuessCount
-    private int guessCount;
+    private final int guessCount;
 
     private int number;
     private int guess;
@@ -27,6 +25,14 @@ public class GameImpl implements Game {
     private int biggest;
     private int remainingGuesses;
     private boolean validNumberRange = true;
+
+    // -- constructors --
+
+    @Autowired
+    public GameImpl(NumberGenerator numberGenerator, @GuessCount int guessCount) {
+        this.numberGenerator = numberGenerator;
+        this.guessCount = guessCount;
+    }
 
     // -- init --
     @PostConstruct
--- core/src/main/java/ihs/learnprogramming/config/GameConfig.java
+++ core/src/main/java/ihs/learnprogramming/config/GameConfig.java
@@ -5,10 +5,12 @@ import ihs.learnprogramming.MaxNumber;
 import ihs.learnprogramming.MinNumber;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@ComponentScan(basePackages = "ihs.learnprogramming")
 @PropertySource("classpath:config/game.properties")
 public class GameConfig {
 
