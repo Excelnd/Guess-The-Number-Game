87b29e07a6e94147dd839298e25eef460351d67c
Excelnd <ihsanmpm@gmail.com>
2019-11-20 22:33:11 +0530

    Thymeleaf WebConfig

---
+++ web/src/main/java/ihs/learnprogramming/util/ViewNames.java
@@ -0,0 +1,10 @@
+package ihs.learnprogramming.util;
+
+public final class ViewNames {
+
+    // -- constants --
+    public static final String HOME = "home";
+
+    // -- constructors --
+    private ViewNames() {}
+}
